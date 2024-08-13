29ce16ac34769e942fdb5df2f8acf472cc9d3a3d
Caleb Matthews <calebm@Deeps-MBP-2.attlocal.net>
2024-08-12 23:35:57 -0500

    Fix 2.0 pt2

--- Gary_Llama/Gary_Llama-Bridging-Header.h
+++ Gary_Llama/Gary_Llama-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "/Users/calebm/Gary_Llama/Gary_Llama/llama.cpp/include/llama.h"
-#include "/Users/calebm/Gary_Llama/Gary_Llama/llama.cpp/ggml/include/ggml.h"
+#import "../llama.cpp/include/llama.h"
+#include "../llama.cpp/ggml/include/ggml.h"
 
 
